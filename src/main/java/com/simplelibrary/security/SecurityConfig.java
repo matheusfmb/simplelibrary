ac3e6133c6ba31e4ddb298fd95b76221b3ce9c2e
Matheus Felipe <matheusbarros9107@gmail.com>
2023-07-04 10:44:39 -0300

    Basic SecurityConfig
    configuração básica de segurança

--- src/main/java/com/simplelibrary/security/SecurityConfig.java
+++ src/main/java/com/simplelibrary/security/SecurityConfig.java
@@ -1,9 +1,46 @@
 package com.simplelibrary.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        http.csrf((csrf) -> csrf.disable())
+        	.authorizeHttpRequests((authorizeHttpRequests) ->
+ 				authorizeHttpRequests
+ 					.requestMatchers(HttpMethod.GET, "/**").permitAll()
+ 					.anyRequest().authenticated());
+		return http.build();
+	}
+
+	@Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+	
+
 
 }
